d6474cd4c577907e2cf730c24727c6fc7722e4aa
basuony <01096465212345678>
2019-07-03 20:34:11 +0200

    fetch Data to Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
-
+import 'package:flutter/cupertino.dart';
+import 'package:intl/intl.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'Model/WeatherModel.dart';
@@ -12,7 +13,6 @@ Future<WeatherModel> getWeatherModel(String lat, String lon) async {
     var result = json.decode(response.body);
     print("=============${result.toString()}================");
     var model = WeatherModel.fromJson(result);
-
 
     return model;
   } else {
@@ -36,21 +36,70 @@ class _MyAppState extends State<MyApp> {
       theme: ThemeData.light(),
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Weather MB"),
+          title: Center(
+            child: Text("Weather MB"),
+          ),
         ),
         body: Container(
           decoration: BoxDecoration(color: Colors.white),
           child: Center(
             child: FutureBuilder<WeatherModel>(
               future: getWeatherModel("31.00333", "31.38166"),
-              builder: (context, snapshot)
-              {
-                      if(snapshot.hasData)
-                        return Text("Location Name : ${snapshot.data.name}",style: TextStyle(fontSize: 30,));
-                      else if (snapshot.hasError)
-                        return Text("${snapshot.error}",style: TextStyle(fontSize: 30,color: Colors.red));
-                      return CircularProgressIndicator();
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  WeatherModel model = snapshot.data;
+                  //format Date
+                  var fm = new DateFormat("HH:mm dd EEE MM yyyy");
+                  var fm_hour = DateFormat.Hm();
 
+                  return Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      //----------------Location-----------------//
+                      Text(
+                        "Weather in ${model.name}",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 20.0),
+                      ),
+                      //-----------Create Row  Image and Temperature-----------//
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>
+                        [
+                          Image.network("https://openweathermap.org/img/wn/${model.weather[0].icon}.png"),
+                          Text(
+                            "${model.main.temp} °C",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 20.0),
+                          )
+                        ],
+                      ),
+                      //---------------Date Time----------------//
+                Text(
+                 "${fm.format(new DateTime.fromMillisecondsSinceEpoch((model.dt*1000),isUtc: true))}",
+                style: TextStyle(
+                fontWeight: FontWeight.bold, fontSize: 20.0),
+                ),
+                 //-------------------Speed/Deg--------------------//
+                 Text("wind(Speed/Deg) : ${model.wind.speed}/${model.wind.deg}",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+                 //-----------------Pressure-----------//
+                 Text("Pressure : ${model.main.pressure} hpa",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+                 //--------------------Humidity---------//
+                 Text("Humidity : ${model.main.humidity} %",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+                 //-----------------Sunrise-------------//
+                 Text("Sunrise : ${fm_hour.format(DateTime.fromMillisecondsSinceEpoch((model.sys.sunrise*1000),isUtc: true))} ",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+                 //------------------sunset---------------//
+                 Text("Sunrise : ${fm_hour.format(DateTime.fromMillisecondsSinceEpoch((model.sys.sunset*1000),isUtc: true))} ",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+                  //------------------
+                 Text("GeoCode : [${model.coord.lat}/${model.coord.lon}]",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+
+
+                    ],
+                  );
+                } else if (snapshot.hasError)
+                  return Text("${snapshot.error}",
+                      style: TextStyle(fontSize: 30, color: Colors.red));
+                return CircularProgressIndicator();
               },
             ),
           ),
